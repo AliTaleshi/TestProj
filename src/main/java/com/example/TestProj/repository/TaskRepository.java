d046047afe83f5425296befdd293d5233403bfea
ali.taleshi <ali.taleshi@nak-mci.ir>
2025-05-02 00:48:13 +0330

    Add TaskRepository interface

---
+++ src/main/java/com/example/TestProj/repository/TaskRepository.java
@@ -0,0 +1,9 @@
+package com.example.TestProj.repository;
+
+import com.example.TestProj.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
